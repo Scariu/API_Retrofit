2aca208fea4283ee43304393b51c10e2c06d3010
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-02 19:46:56 -0400

    Se crea FirstFragmentListaTerrenos.

---
+++ app/src/main/java/com/example/api_retrofit/presentation/FirstFragmentListaTerrenos.kt
@@ -0,0 +1,19 @@
+package com.example.api_retrofit.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.api_retrofit.R
+
+class FirstFragmentListaTerrenos : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first_lista_terrenos, container, false)
+    }
+
+}
