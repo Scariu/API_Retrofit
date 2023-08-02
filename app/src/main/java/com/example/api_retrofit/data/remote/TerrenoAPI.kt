83ca942242e862667c1ad963b97bcf7fb2ded153
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-01 21:43:59 -0400

    Se implementa interface TerrenoAPI.

--- app/src/main/java/com/example/api_retrofit/data/remote/TerrenoAPI.kt
+++ app/src/main/java/com/example/api_retrofit/data/remote/TerrenoAPI.kt
@@ -1,4 +1,10 @@
 package com.example.api_retrofit.data.remote
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface TerrenoAPI {
+    @GET("realestate")
+    //Se ejecuta un GET en un ulr realestate y retorna con la lista
+    suspend fun getData(): Response<List<Terreno>>
 }
