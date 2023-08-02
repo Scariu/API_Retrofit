723e0d22d2cd58ce8c97d90e775b2b2377b1ac63
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-01 21:39:12 -0400

    Se implementa class Terreno.

--- app/src/main/java/com/example/api_retrofit/data/remote/Terreno.kt
+++ app/src/main/java/com/example/api_retrofit/data/remote/Terreno.kt
@@ -1,4 +1,11 @@
 package com.example.api_retrofit.data.remote
 
-class Terreno {
-}
+import com.google.gson.annotations.SerializedName
+
+class Terreno(
+    val id: String,
+    //@SerializedName le dice como se llama en la API (formato JSON)
+    @SerializedName("price") val price: Long,
+    @SerializedName("type") val type: String,
+    @SerializedName("img_src") val image: String
+)
