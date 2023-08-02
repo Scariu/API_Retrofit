1a074457af4d0f78969326171e7a5d4c1ac2034d
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-01 21:56:10 -0400

    Se implementa class TerrenoRetrofitClient.

--- app/src/main/java/com/example/api_retrofit/data/remote/TerrenoRetrofitClient.kt
+++ app/src/main/java/com/example/api_retrofit/data/remote/TerrenoRetrofitClient.kt
@@ -1,4 +1,19 @@
 package com.example.api_retrofit.data.remote
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class TerrenoRetrofitClient {
+    companion object {
+        private const val URL_BASE =
+            "https://android-kotlin-fun-mars-server.appspot.com/"
+
+        fun getRetrofitClientTerreno(): TerrenoAPI {
+            val mRetrofit = Retrofit.Builder()
+                .baseUrl(URL_BASE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return mRetrofit.create(TerrenoAPI::class.java)
+        }
+    }
 }
