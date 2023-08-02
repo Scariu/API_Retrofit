d5b1c4aaf2c1a6d15ca744512c6ca9c881a3d24a
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-02 19:43:59 -0400

    Se implementa class TerrenoViewModel.

--- app/src/main/java/com/example/api_retrofit/presentation/TerrenoViewModel.kt
+++ app/src/main/java/com/example/api_retrofit/presentation/TerrenoViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.api_retrofit.presentation
 
-class TerrenoViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.api_retrofit.data.Repositorio
+import com.example.api_retrofit.data.remote.TerrenoRetrofitClient
+import kotlinx.coroutines.launch
+
+class TerrenoViewModel(application: Application): AndroidViewModel(application) {
+    private val repositorio: Repositorio
+    init {
+        val api = TerrenoRetrofitClient.getRetrofitClientTerreno()
+        repositorio = Repositorio(api)
+    }
+    fun getListaTerrenos() = viewModelScope.launch{
+        repositorio.cargarTerreno()
+    }
 }
