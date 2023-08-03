2ddf97f2c8bba482395f636d73deb6f02aa33575
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-02 21:33:03 -0400

    Se implementa class Adapter y se agregan dependencias de coil.

--- app/src/main/java/com/example/api_retrofit/presentation/Adapter.kt
+++ app/src/main/java/com/example/api_retrofit/presentation/Adapter.kt
@@ -1,4 +1,33 @@
 package com.example.api_retrofit.presentation
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.api_retrofit.data.remote.Terreno
+import com.example.api_retrofit.databinding.ItemBinding
+
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+    lateinit var binding: ItemBinding
+    private val listaTerrenos = mutableListOf<Terreno>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+        binding = ItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+        val terreno = listaTerrenos[position]
+        holder.bind(terreno)
+    }
+
+    override fun getItemCount(): Int {
+        return listaTerrenos.size
+    }
+
+    class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(terreno: Terreno) {
+            binding.imagenTerreno.load(terreno.image)
+        }
+    }
 }
