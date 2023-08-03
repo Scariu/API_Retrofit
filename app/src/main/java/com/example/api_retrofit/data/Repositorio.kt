42fecdb6b31a05860aaa7314d6f06dc1507bcc41
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-02 20:43:54 -0400

    Se implementa class Repositorio.

--- app/src/main/java/com/example/api_retrofit/data/Repositorio.kt
+++ app/src/main/java/com/example/api_retrofit/data/Repositorio.kt
@@ -4,6 +4,14 @@ import com.example.api_retrofit.data.remote.Terreno
 import com.example.api_retrofit.data.remote.TerrenoAPI
 
 class Repositorio(private val terrenoAPI: TerrenoAPI) {
-    suspend fun cargarTerreno() {
+    suspend fun cargarTerreno(): List<Terreno> {
+        val response = terrenoAPI.getData()
+        if (response.isSuccessful) {
+            val resp = response.body()
+            resp?.let {
+                return it
+            }
+        }
+        return emptyList()
     }
 }
