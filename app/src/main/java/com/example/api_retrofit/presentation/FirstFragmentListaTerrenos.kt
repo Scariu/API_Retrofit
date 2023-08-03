bde6d1b6027b5f6e3b852d40707fb239699a5372
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-02 20:35:08 -0400

    Se implementa función a botón en FirstFragmentListaTerrenos.

--- app/src/main/java/com/example/api_retrofit/presentation/FirstFragmentListaTerrenos.kt
+++ app/src/main/java/com/example/api_retrofit/presentation/FirstFragmentListaTerrenos.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.api_retrofit.R
 import com.example.api_retrofit.databinding.FragmentFirstListaTerrenosBinding
 
 class FirstFragmentListaTerrenos : Fragment() {
     lateinit var binding: FragmentFirstListaTerrenosBinding
-
+    private val viewModel: TerrenoViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFirstListaTerrenosBinding.inflate(layoutInflater)
+        initListeners()
         return binding.root
+    }
+
+    private fun initListeners() {
+        binding.btnCargar.setOnClickListener{
+            viewModel.getListaTerrenos()
+        }
     }
 }
