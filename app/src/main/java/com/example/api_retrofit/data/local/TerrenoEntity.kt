e9f80ef874fcea75d7bae62b8fa05c3ebbd89f1f
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-03 20:13:36 -0400

    Se implementa class TerrenoEntity.

--- app/src/main/java/com/example/api_retrofit/data/local/TerrenoEntity.kt
+++ app/src/main/java/com/example/api_retrofit/data/local/TerrenoEntity.kt
@@ -1,4 +1,7 @@
 package com.example.api_retrofit.data.local
 
-class TerrenoEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "tabla_terrenos")
+data class TerrenoEntity(@PrimaryKey var id: String, val price: Long, val type: String, val image: String)
