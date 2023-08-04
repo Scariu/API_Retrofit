fb42b341fb34d326da22f109cc8dbb859054beb1
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-03 20:25:36 -0400

    Se implementa interface TerrenoDAO.

--- app/src/main/java/com/example/api_retrofit/data/local/TerrenoDAO.kt
+++ app/src/main/java/com/example/api_retrofit/data/local/TerrenoDAO.kt
@@ -1,4 +1,15 @@
 package com.example.api_retrofit.data.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface TerrenoDAO {
+    @Insert
+    suspend fun insertarTerrenoDao(terrenoEntity: TerrenoEntity)
+
+    @Query("SELECT * FROM tabla_terrenos order by id ASC")
+    fun getTodosTerrenosDao(): LiveData<List<TerrenoEntity>>
 }
